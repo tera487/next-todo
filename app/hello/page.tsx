6885aba05fd5d76f5a2542b4aa173cc23b9da367
tera <tera.1999.kagayaku@ezweb.ne.jp>
2023-07-02 15:35:16 +0900

    add: material ui install

--- app/hello/page.tsx
+++ app/hello/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "../lib/mui";
+import Link from "next/link";
+
 export default function Hello() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello World</h1>
+      <Button variant="contained">
+        <Link href="/">Back</Link>
+      </Button>
     </main>
   );
 }
