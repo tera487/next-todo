2a536ad9bc4d60e93767f42c4727388bafc70a4b
tera <tera.1999.kagayaku@ezweb.ne.jp>
2023-07-15 12:08:51 +0900

    modify: レイアウト修正

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,19 @@ import List from "@/app/components/elements/list";
 import TodoForm from "@/app/components/elements/todoForm";
 import model from "@/app/features/todo";
 
-const initialTodoList: model[] = [{ id: 1, title: "test", status: "doing" }];
-const initialTodoId: number = 2;
+const initialTodoList: model[] = [
+  { id: 1, title: "test1", status: "done" },
+  { id: 2, title: "test2", status: "doing" },
+  { id: 3, title: "test3", status: "doing" },
+  { id: 4, title: "tes4", status: "doing" },
+];
+const initialTodoId: number = 5;
 
 export default function Home() {
   const [todoList, setTodoList] = React.useState(initialTodoList);
   const [todoId, setTodoId] = React.useState(initialTodoId);
 
-  function handleToggle(value: number) {
+  const handleToggle = (value: number) => () => {
     let newTodoList: model[] = [...todoList];
     if (newTodoList[value].status == "doing") {
       newTodoList[value].status = "done";
@@ -21,7 +26,7 @@ export default function Home() {
       newTodoList[value].status = "doing";
     }
     setTodoList(newTodoList);
-  }
+  };
 
   function handleAddTodo(value: string) {
     let newTodoList: model[] = [...todoList];
@@ -36,11 +41,11 @@ export default function Home() {
     setTodoId(newTodoId + 1);
   }
 
-  function handleDeleteTodo(value: number) {
+  const handleDeleteTodo = (value: number) => () => {
     let newTodoList: model[] = [...todoList];
     newTodoList.splice(value, 1);
     setTodoList(newTodoList);
-  }
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -53,7 +58,7 @@ export default function Home() {
         handleToggle={handleToggle}
         handleDeleteTodo={handleDeleteTodo}
       />
-      <Link href="/hello">Log</Link>
+      <Link href="/features/log">Log</Link>
     </main>
   );
 }
